90ead98e78dcfd89e8c4793de410f2e5904a6e0c
alexaleluia12 <alexaleluiaforgit@gmail.com>
2015-02-25 21:32:31 -0300

    change getFile to mock version

--- tests/commonWord_test.py
+++ tests/commonWord_test.py
@@ -1,46 +1,36 @@
 #!/usr/bin/env python3
 
-
 import unittest
-import os, sys
+from unittest import mock
+import os
+import sys
 
 sys.path.insert(1, os.path.join('..'))
 import mod.commonWord as cw
-from mod.myErrorException import FileEmpty
-
-
-
-FILE =  os.path.join('aux', 'romeo.txt')
-FILE2 = os.path.join('aux', 'self.txt')
-FILEEMP = os.path.join('aux', 'music.txt')
 
 class CommonWordTest(unittest.TestCase):    
-
-    def test_getFile_str(self):
-        self.assertIsInstance(cw.getFile(FILE), str)      
-      
-    def test_getFile_exception(self):
-        self.assertRaises(FileEmpty, cw.getFile, FILEEMP)
-
-    #--
+    
+    @mock.patch.object(cw, 'getFile', autospec=True)
+    def test_getFile(self, mock_getFile):
+        self.assertRaises(TypeError, mock_getFile, 'fakeFile', 'fake',
+                          msg="Failed to raise error, the interface of \
+                          'getFile' change")
+                          
+        mock_getFile.return_value = 'the mock content file string'
+        
+        self.assertEqual(mock_getFile('fileName'),
+                         'the mock content file string')
+                                   
     def test_mapWordCount(self):
-        words = "it it no can"
-        self.assertDictEqual(cw.mapWordCount(words), {'it':2, 'no': 1, 'can': 1})
+        valStr = "other   x in : the; the"
+        self.assertEqual(cw.mapWordCount(valStr),
+                         {'the': 2, 'x': 1, 'other': 1, 'in': 1})
     
-    #--
     def test_getWin(self):
-        testDict = {'it':2, 'no': 1, 'can': 1}
-        self.assertTupleEqual(cw.getWin(testDict), ('it', 2))
-    
-    #--
-    def test_main(self):        
-        self.assertIsInstance(cw.main(FILE), tuple)
-        self.assertTupleEqual(cw.main(FILE2), ('self', 4))
-        
-        
-
+        valDict = {'a': 2, 'b': 34, 'c': 33}
+        self.assertEqual(cw.getWin(valDict), ('b', 34))
     
 if __name__ == "__main__":
     unittest.main()        
     # run test: $ python3 commonWord_test.py -v
-    
+   

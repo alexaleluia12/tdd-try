5c6ce32dea25d4d0ef641443176afb8406ec29ac
alexaleluia12 <alexaleluiaforgit@gmail.com>
2015-02-27 15:05:58 -0300

    change the code organization

--- tests/commonWord_test.py
+++ tests/commonWord_test.py
@@ -12,19 +12,24 @@ class CommonWordTest(unittest.TestCase):
     
     @mock.patch.object(cw, 'getFile', autospec=True)
     def test_getFile(self, mock_getFile):
-        self.assertRaises(TypeError, mock_getFile, 'fakeFile', 'fake',
-                          msg="Failed to raise error, the interface of \
-                          'getFile' change")
+        self.assertRaises(
+            TypeError, mock_getFile, 'fakeFile', 'fake',
+            msg="Failed to raise error, the interface of 'getFile' change"
+        )
                           
         mock_getFile.return_value = 'the mock content file string'
         
-        self.assertEqual(mock_getFile('fileName'),
-                         'the mock content file string')
+        self.assertEqual(
+            mock_getFile('fileName'),
+            'the mock content file string'
+        )
                                    
     def test_mapWordCount(self):
         valStr = "other   x in : the; the"
-        self.assertEqual(cw.mapWordCount(valStr),
-                         {'the': 2, 'x': 1, 'other': 1, 'in': 1})
+        self.assertEqual(
+            cw.mapWordCount(valStr),
+            {'the': 2, 'x': 1, 'other': 1, 'in': 1}
+        )
     
     def test_getWin(self):
         valDict = {'a': 2, 'b': 34, 'c': 33}

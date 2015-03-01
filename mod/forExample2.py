ae3363d3c0c6a1d171ff472fa8b722e49deeebcd
alexaleluia12 <alexaleluiaforgit@gmail.com>
2015-03-01 19:26:58 -0300

    clear the code and add more one assert

--- mod/forExample2.py
+++ mod/forExample2.py
@@ -2,14 +2,11 @@
 
 import sys
 
-
 try: 
     from . import forExample1 as fore
 except:
     import forExample1 as fore
-    
-# eu nao gosto desse estilo de import mas foi o unico que funcionou
-# tanto no test quanto na excecao narmal do arquivo    
+
 """
 maior = None  
 print 'Anters: ', maior
@@ -20,7 +17,7 @@ for intere in [3, 41, 12, 9, 74, 15]:
     print "loop: ", intere, maior
 print "maior numero no vetor: ", maior	
 """
-# main(list)→ int (retorna o maior elemento da list)
+# main(list)→ int (return the maximum value on list)
 
 def main(valList):
     return max(valList)
@@ -32,9 +29,7 @@ if __name__ == '__main__':
 	except ValueError as err:
 	    print('Suport only int \n{0}'.format(err))
 	maxVal = main(val)
-	print('Max value: {}'.format(maxVal))
+	print('Max value: {0}'.format(maxVal))
 	
 	# run: $ python3 forExample2.py [1,2,3]
-	
-
-    
+	    
--- tests/forExample2_test.py
+++ tests/forExample2_test.py
@@ -10,12 +10,9 @@ class ForExample2Test(unittest.TestCase):
     
     def test_main(self):
         self.assertEqual(fore.main([1,2,3,906,90]), 906)
+        self.assertEqual(fore.main([-1,-2,-45,-1,0]), 0)
         
 
 if __name__ == '__main__':
 	unittest.main()
-
-        
-    
-
 

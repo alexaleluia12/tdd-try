3138407398d06c13dcbfd8ad5b647eafe7275619
alexaleluia12 <alexaleluiaforgit@gmail.com>
2015-03-08 10:25:37 -0300

    remove myErrorException, I'm not wear this content any more

--- mod/myErrorException.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-#-*- coding:utf-8 -*-
-
-
-class FileEmpty(Exception):
-    def __init__(self, msg):
-        self.value = msg
-    def __str__(self):
-        return repr(self.value)
